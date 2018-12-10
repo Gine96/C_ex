4471453f5a47174cd22936ca91916efb54ff2e74
Gine96 <eliacotticottini96@gmail.com>
2018-12-10 22:30:45 +0100

    Update es20.c

--- es20.c
+++ es20.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define DIMR 10
 #define DIMC 12
 
-int somma(int mat[][DIMC], int dimr, int c){
-	int i;
-	for(i=0;i<dimr-1;i++)
-		mat[i][c]+=mat[i+1][c];
+int somma(int mat[][DIMC], int dimc, int r){
+    int i,somma=0;
+    for(i=0;i<dimc;i++)
+        somma+=mat[r][i];
+    return somma;
 }
 
 int main(void){
-	int mat[DIMR][DIMC],i,j, somma_riga;
-	for(i=0;i<DIMR;i++){
-		for(j=0;j<DIMC;j++){
-			mat[i][j]=rand()%101-50;
-			printf("%d ",mat[i][j]);
-		}
-		printf("\n");
-	}
-	for(i=0;i<DIMC;i++){
-		somma_riga=somma(mat,DIMR,i);
-		printf("Somma riga #%d: %d\n", i+1, somma_riga);
-	}
-	
-	
-	return 0;
+	srand(time(NULL));
+    int mat[DIMR][DIMC],i,j, somma_riga;
+    for(i=0;i<DIMR;i++){
+        for(j=0;j<DIMC;j++){
+            mat[i][j]=rand()%101-50;
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<DIMR;i++){
+        somma_riga=somma(mat,DIMC,i);
+        printf("Somma riga #%d: %d\n", i+1, somma_riga);
+    }
+
+
+    return 0;
 }
